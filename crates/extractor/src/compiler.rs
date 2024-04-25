a53e1a2b46d4d83cd3f7df924b6026203010cc4a
shogochiai <shogo.ochiai@protonmail.com>
2024-04-25 12:05:58 +0900

    first failing test

--- crates/extractor/src/compiler.rs
+++ crates/extractor/src/compiler.rs
@@ -17,24 +17,16 @@ use serde_json::Value;
 
 pub struct Compiler {
     solc_path: String,
-    base_slot_ast_cache: Option<String>,
-    storage_layout_ast_cache: Option<String>,
 }
 
 impl Compiler {
     pub fn new(solc_path: String) -> Self {
         Self {
             solc_path,
-            base_slot_ast_cache: None,
-            storage_layout_ast_cache: None,
         }
     }
 
     pub fn prepare_base_slots(&mut self) -> Result<Value, Box<dyn std::error::Error>> {
-        if let Some(ref cache) = self.base_slot_ast_cache {
-            return Ok(serde_json::from_str(cache)?);
-        }
-
         let solc_opts = "./solcBaseSlotsOpts.json";
         let output = Command::new(&self.solc_path)
             .arg("--standard-json")
@@ -44,16 +36,10 @@ impl Compiler {
         let stdout = String::from_utf8(output.stdout)?;
         let parsed: Value = serde_json::from_str(&stdout)?;
 
-        self.base_slot_ast_cache = Some(stdout);
-
         Ok(parsed)
     }
 
     pub fn prepare_storage_layout(&mut self) -> Result<Value, Box<dyn std::error::Error>> {
-        if let Some(ref cache) = self.storage_layout_ast_cache {
-            return Ok(serde_json::from_str(cache)?);
-        }
-
         let solc_opts = "./solcLayoutOpts.json";
         let output = Command::new(&self.solc_path)
             .arg("--standard-json")
@@ -63,8 +49,24 @@ impl Compiler {
         let stdout = String::from_utf8(output.stdout)?;
         let parsed: Value = serde_json::from_str(&stdout)?;
 
-        self.storage_layout_ast_cache = Some(stdout);
-
         Ok(parsed)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prepare_storage_layout() {
+        let mut compiler = Compiler::new("solc".to_string());
+        let storage_layout_blob = match compiler.prepare_storage_layout() {
+            Ok(blob) => blob,
+            Err(err) => {
+                panic!("Error preparing storage layout: {}", err);
+            }
+        };
+
+        assert_eq!(storage_layout_blob, 1);
     }
 }
